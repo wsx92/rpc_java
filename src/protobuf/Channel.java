051b5948fb89b7da84f9f03f82b94ada71cf2405
wsx92 <543993160@qq.com>
2019-06-06 17:50:48 +0800

    channel

---
+++ src/protobuf/Channel.java
@@ -0,0 +1,10 @@
+package protobuf;
+
+import com.google.protobuf.*;
+
+public class Channel implements RpcChannel {
+    @Override
+    public void callMethod(Descriptors.MethodDescriptor methodDescriptor, RpcController rpcController, Message message, Message message1, RpcCallback<Message> rpcCallback) {
+
+    }
+}
