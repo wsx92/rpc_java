ae4e394af6426e30e60637e4deee1a3737a4372d
wsx92 <543993160@qq.com>
2019-06-13 20:55:39 +0800

    modify encode

--- src/protobuf/ProtoBufDecoder.java
+++ src/protobuf/ProtoBufDecoder.java
@@ -4,26 +4,40 @@ import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
-import protobuf.proto.Rpc;
 
 public class ProtoBufDecoder extends CumulativeProtocolDecoder {
     @Override
     protected boolean doDecode(IoSession ioSession, IoBuffer ioBuffer, ProtocolDecoderOutput protocolDecoderOutput) throws Exception {
-        if(ioBuffer.remaining() < 4){
+        if(ioBuffer.remaining() < 12){
             return false;
         }
         else {
             ioBuffer.mark();
+            byte dummy1 = ioBuffer.get();
+            byte dummy2 = ioBuffer.get();
+            byte dummy3 = ioBuffer.get();
+            byte dummy4 = ioBuffer.get();
+            if(Integer.valueOf('P').byteValue() != dummy1 || Integer.valueOf('R').byteValue() != dummy2 || Integer.valueOf('P').byteValue() != dummy3 || Integer.valueOf('C').byteValue() != dummy4) {
+                throw new RpcException("unsupported proto");
+            }
             int bodyLength = ioBuffer.getInt();
+            int metaLength = ioBuffer.getInt();
+            if(metaLength > bodyLength) {
+                throw new RpcException("metaLength=" + metaLength + " is bigger than bodyLength=" + bodyLength);
+            }
             if(ioBuffer.remaining() < bodyLength){
                 ioBuffer.reset();
                 return false;
             }
             else{
-                byte[] bodyBytes = new byte[bodyLength];
-                ioBuffer.get(bodyBytes);
-                Rpc.RpcMeta meta = Rpc.RpcMeta.parseFrom(bodyBytes);
-                protocolDecoderOutput.write(meta);
+                byte[] metaBytes = new byte[metaLength];
+                byte[] payLoadBytes = new byte[bodyLength - metaLength];
+                ioBuffer.get(metaBytes);
+                ioBuffer.get(payLoadBytes);
+                IoBuffer meta = IoBuffer.wrap(metaBytes);
+                IoBuffer payLoad = IoBuffer.wrap(payLoadBytes);
+                InputMessage message = new InputMessage(meta, payLoad);
+                protocolDecoderOutput.write(message);
                 return true;
             }
         }
--- src/protobuf/ProtoBufEncoder.java
+++ src/protobuf/ProtoBufEncoder.java
@@ -1,7 +1,5 @@
 package protobuf;
 
-import com.google.protobuf.Message;
-import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolEncoderAdapter;
 import org.apache.mina.filter.codec.ProtocolEncoderOutput;
@@ -10,13 +8,6 @@ public class ProtoBufEncoder extends ProtocolEncoderAdapter {
 
     @Override
     public void encode(IoSession ioSession, Object o, ProtocolEncoderOutput protocolEncoderOutput) throws Exception {
-        Message message = (Message) o;
-        byte[] bytes = message.toByteArray();
-        int length = bytes.length;
-        IoBuffer buffer = IoBuffer.allocate(length + 4);
-        buffer.putInt(length);
-        buffer.put(bytes);
-        buffer.flip();
-        protocolEncoderOutput.write(buffer);
+        protocolEncoderOutput.write(o);
     }
 }
