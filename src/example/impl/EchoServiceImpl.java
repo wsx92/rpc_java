57e5fcd985deaf03291879889e01f649dcc1895d
wsx92 <543993160@qq.com>
2019-06-16 23:37:33 +0800

    modify log

--- src/example/impl/EchoServiceImpl.java
+++ src/example/impl/EchoServiceImpl.java
@@ -3,13 +3,13 @@ package example.impl;
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
 import example.proto.Echo;
-import org.slf4j.LoggerFactory;
-
+import log.Log;
+import log.LogCategory;
 
 public class EchoServiceImpl extends Echo.EchoService {
     @Override
     public void echo(RpcController controller, Echo.EchoRequest request, RpcCallback<Echo.EchoResponse> done) {
-        LoggerFactory.getLogger(EchoServiceImpl.class).info(request.getMessage());
+        Log.info(LogCategory.Server, request.getMessage());
         Echo.EchoResponse.Builder builder = Echo.EchoResponse.newBuilder();
         builder.setMessage(request.getMessage());
         done.run(builder.build());
--- src/example/impl/LoginServiceImpl.java
+++ src/example/impl/LoginServiceImpl.java
@@ -2,14 +2,15 @@ package example.impl;
 
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
-import org.slf4j.LoggerFactory;
+import log.Log;
+import log.LogCategory;
 import example.proto.Login;
 
 public class LoginServiceImpl extends Login.LoginService {
     @Override
     public void login(RpcController controller, Login.LoginRequest request, RpcCallback<Login.LoginResponse> done) {
-        LoggerFactory.getLogger(LoginServiceImpl.class).info(request.getUser());
-        LoggerFactory.getLogger(LoginServiceImpl.class).info(request.getPassword());
+        Log.info(LogCategory.Client, request.getUser());
+        Log.info(LogCategory.Client, request.getPassword());
         Login.LoginResponse.Builder builder = Login.LoginResponse.newBuilder();
         builder.setResult("success");
         done.run(builder.build());
